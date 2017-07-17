04a866cc36ff16cf1c4c141998f8758b807efbc7
Anderson Home <agsalle@gmail.com>
2017-07-16 21:48:32 -0300

    git innit

---
+++ seletores/seletores.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Anderson on 16/07/2017.
+ */
+$(function(){
+
+    var selecionarTodosUsuarios = $('#selecao-todos-usuarios');
+    var selecionarUsuarios = $('.js-selecao-usuario');
+
+    selecionarUsuarios.on('click',function () {
+
+    });
+
+});
